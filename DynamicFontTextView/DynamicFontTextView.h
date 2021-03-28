65892616795341ad35a62c0cdf1f5292c029506e
Ari Tamunomieni <aribiton@betazoo.com.ng>
2021-03-28 10:52:35 +0100

    first commit

---
+++ DynamicFontTextView/DynamicFontTextView.h
@@ -0,0 +1,19 @@
+//
+//  DynamicFontTextView.h
+//  DynamicFontTextView
+//
+//  Created by thetaeo on 28/03/2021.
+//  Copyright © 2021 Button Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DynamicFontTextView.
+FOUNDATION_EXPORT double DynamicFontTextViewVersionNumber;
+
+//! Project version string for DynamicFontTextView.
+FOUNDATION_EXPORT const unsigned char DynamicFontTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicFontTextView/PublicHeader.h>
+
+
---
+++ DynamicFontTextViewExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicFontTextViewExamples
+//
+//  Created by thetaeo on 28/03/2021.
+//  Copyright © 2021 Button Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
